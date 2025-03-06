ab7c0c6865761356205e6458a503b581ba5bceee
fonseca-plx <pedrolucasxf@gmail.com>
2025-03-06 01:02:54 -0300

    Create Estoque.java

---
+++ MeuMenu/src/entities/Estoque.java
@@ -0,0 +1,10 @@
+package entities;
+
+import java.util.List;
+
+public class Estoque {
+
+    List<Ingrediente> ingredientes;
+
+    // TODO add metodos e add atributo quantidade na classe Ingrediente
+}
